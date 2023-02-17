791ad903bfc7700dd40a7bd3acaac16a2515131e
AkankshaChaudhari24 <akankshac@sahaj.ai>
2023-02-17 10:01:15 +0530

    modify parking lot tests

--- src/test/kotlin/parkingLotTest.kt
+++ src/test/kotlin/parkingLotTest.kt
@@ -1,19 +1,25 @@
-import Models.Ticket
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class parkingLotTest {
-    private val parkingLot = parkingLot()
-    private val ticket = parkingLot.park()
 
     @Test
     fun park() {
+
+        val parkingLot = parkingLot()
+        val ticket = parkingLot.park()
+
+
         assertEquals(1, ticket!!.ticketNumber)
         assertEquals(1, ticket.spotNumber)
     }
 
     @Test
     fun unPark() {
+        val parkingLot = parkingLot()
+        val ticket = parkingLot.park()
+
+
         val receipt = parkingLot.unPark(ticket!!)
         assertEquals(1,receipt.receiptNumber)
         assertEquals(0,receipt.fees)
